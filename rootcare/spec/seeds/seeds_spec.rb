381760dcfd2a06e0ea9dbd48af8c9182fb23fab5
timproctor <timandcamiproctor@gmail.com>
2017-05-05 16:20:46 -0600

    made a spec for creating a driver when database loads

--- rootcare/spec/seeds/seeds_spec.rb
+++ rootcare/spec/seeds/seeds_spec.rb
@@ -9,10 +9,15 @@ describe "seeds" do
     @data_formatted = @data_lines[0].split(/\r/).map do |entry|
                         entry.split
                       end
+
+    seed_data
   end
 
   it "creates a driver given driver command" do
-    
+
+    driver = Driver.create!(driver_attributes)
+
+    expect(driver.name).to eq("rex")
   end
 
 end
