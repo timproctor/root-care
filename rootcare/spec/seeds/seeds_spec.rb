8433f3fa7ae05fc41078180f1ee665c9f8b2f2a7
timproctor <timandcamiproctor@gmail.com>
2017-05-05 00:11:21 -0600

    opening file correctly now

--- rootcare/spec/seeds/seeds_spec.rb
+++ rootcare/spec/seeds/seeds_spec.rb
@@ -2,11 +2,13 @@ require "rails_helper"
 
 describe "seeds" do
   before do
-    byebug
-
+    @data_lines = File.open("spec/fixtures/files/driver_and_trip_data.prn", "r") do |datafile|
+      datafile.readlines
+    end
   end
-  it "creates a driver given driver command" do
 
+  it "creates a driver given driver command" do
+    byebug
   end
 
 end
