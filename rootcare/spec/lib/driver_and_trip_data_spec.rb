78ffcbbe0e71b2c98beef4a6fe60092d17314f81
timproctor <timandcamiproctor@gmail.com>
2017-05-04 22:59:05 -0600

    fixed file typo, one nested too many

--- rootcare/spec/lib/driver_and_trip_data_spec.rb
+++ rootcare/spec/lib/driver_and_trip_data_spec.rb
@@ -2,6 +2,6 @@ require "rails_helper"
 
 RSpec.describe "file" do
   it "reads driver_and_trip_data file" do
-    expect(file_fixture("driver_and_trip_data").read).to eq("driver")
+    expect(file_fixture_upload("driver_and_trip_data.prn").read).to eq("driver")
   end
 end
