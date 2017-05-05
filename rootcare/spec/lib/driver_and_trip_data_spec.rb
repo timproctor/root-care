1a6b80e9c3e5e87a816ea90858fa81f055c48624
timproctor <timandcamiproctor@gmail.com>
2017-05-04 23:03:50 -0600

    deleted spec/lib dir

--- rootcare/spec/lib/driver_and_trip_data_spec.rb
+++
@@ -1,7 +0,0 @@
-require "rails_helper"
-
-RSpec.describe "file" do
-  it "reads driver_and_trip_data file" do
-    expect(file_fixture_upload("driver_and_trip_data.prn").read).to eq("driver")
-  end
-end
