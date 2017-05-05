db1096f7fb44936af36f166b41a1c8a481b0006a
timproctor <timandcamiproctor@gmail.com>
2017-05-05 15:41:05 -0600

    set-up a way to test the data

--- rootcare/spec/lib/file_spec.rb
+++ rootcare/spec/lib/file_spec.rb
@@ -1,7 +1,16 @@
 require "rails_helper"
 
 RSpec.describe "file" do
+  before do
+    @data_lines = File.open("spec/fixtures/files/driver_and_trip_data.prn", "r") do |datafile|
+      datafile.readlines
+    end
+
+    @data_formatted = @data_lines[0].split(/\r/).map do |entry|
+                        entry.split
+                      end
+  end
   it "reads mock data file" do
-    expect(file_fixture("driver_and_trip_data.prn").read).to eq("")
+    expect(@data_formatted[0][1]).to eq("rex")
   end
 end
--- rootcare/spec/seeds/seeds_spec.rb
+++ rootcare/spec/seeds/seeds_spec.rb
@@ -5,10 +5,14 @@ describe "seeds" do
     @data_lines = File.open("spec/fixtures/files/driver_and_trip_data.prn", "r") do |datafile|
       datafile.readlines
     end
+
+    @data_formatted = @data_lines[0].split(/\r/).map do |entry|
+                        entry.split
+                      end
   end
 
   it "creates a driver given driver command" do
-    byebug
+    
   end
 
 end
