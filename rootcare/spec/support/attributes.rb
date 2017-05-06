97ab81d52d20285b49318b3881c65fef4a7db25c
timproctor <timandcamiproctor@gmail.com>
2017-05-05 19:55:19 -0600

    added driver and dirt attributes in spec

---
+++ rootcare/spec/support/attributes.rb
@@ -0,0 +1,17 @@
+def driver_attributes(overrides = {})
+  {
+    name: "rex"
+
+  }.merge(overrides)
+end
+
+def dirt_attributes(overrides = {})
+  {
+    name: "ben",
+    start_time: "1:00",
+    end_time: "2:00",
+    distance: "60.2"
+    
+  }.merge(overrides)
+
+end
