c39a0d3786ce370fe67fac1391479e5ccbba643f
timproctor <timandcamiproctor@gmail.com>
2017-05-06 10:36:08 -0600

    refactored spec to test driver has many dirts through trips

--- rootcare/spec/models/driver_spec.rb
+++ rootcare/spec/models/driver_spec.rb
@@ -9,15 +9,24 @@ RSpec.describe Driver, type: :model do
     expect(driver.errors[:name].any?).to eq(true)
   end
 
-  it "has many trips" do
-    driver = Driver.new(driver_attributes)
-    trip1 = User.new(driver_attributes(name: "rex"))
-    trip2 = User.new(driver_attributes(name: "ben"))
+  it "it has many dirts" do
+    driver = driver.new(driver_attributes)
+    driver2= driver2.new(driver_attributes(name: "ben"))
+    driver3= driver3.new(driver_attributes(name: "cami"))
 
-    driver.dirts.new(name: trip1)
-    driver.dirts.new(name: trip2)
+    dirt = Dirt.new(dirt_attributes(name: "rex"))
+    dirt2 = Dirt.new(dirt_attributes(name: "ben "))
+
+    trip1 = trips.dirt.new(dirt_id: dirt.id, driver_id: driver.id)
+    trip1.driver = driver
+    trip1.save!
+
+    trip2 = trips.dirt.new(dirt_id: dirt2.id, driver_id: driver2.id)
+    trip2.driver = driver2
+    trip2.save!
 
     expect(driver.trips).to include(trip1)
-    expect(driver.trips).to include(trip2)
+    expect(driver2.trips).to include(driver2)
+    expect(driver3.trips).not_to include(driver2)
   end
 end
