a421be7e248d351352de6f56b814b72f1a1b5d9a
timproctor <timandcamiproctor@gmail.com>
2017-05-05 19:58:41 -0600

    fixed a bunch of typos in driver model spec

--- rootcare/app/models/driver.rb
+++ rootcare/app/models/driver.rb
@@ -1,2 +1,3 @@
 class Driver < ApplicationRecord
+  validates :name, presence: true
 end
--- rootcare/spec/models/driver_spec.rb
+++ rootcare/spec/models/driver_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Driver, type: :model do
   it "has a unique name" do
-    driver = Driver.create!(driver_attributes(name: ''))
+    driver = Driver.create(driver_attributes(name: ''))
 
     driver.valid?
 
-    expect(driver.errors[:title].any?).to eq(true)
+    expect(driver.errors[:name].any?).to eq(true)
   end
 end
