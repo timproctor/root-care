4ea8015a0fce3ee69f109c78d09a158fdc22b96c
timproctor <timandcamiproctor@gmail.com>
2017-05-06 20:00:41 -0600

    passing file spec

--- rootcare/spec/lib/file_spec.rb
+++ rootcare/spec/lib/file_spec.rb
@@ -1,9 +1,7 @@
 require "rails_helper"
 
 RSpec.describe "file" do
-  before do
-    seed_data
-  end
-  it "reads mock data file" do
+  it "reads driver_and_trip_data.prn " do
+    expect(file_fixture("driver_and_trip_data.prn").read[0..5]).to eq("driver")
   end
 end
