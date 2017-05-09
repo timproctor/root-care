67d9e53159eb7198db0458007f0a0cb07d7c14ac
timproctor <timandcamiproctor@gmail.com>
2017-05-08 20:54:02 -0600

    first pass at adding supporting methods for #change_in_time

--- rootcare/app/models/dirt.rb
+++ rootcare/app/models/dirt.rb
@@ -3,4 +3,19 @@ class Dirt < ApplicationRecord
 
   validates :driver_id, :start_time, :end_time, :distance, presence: true
 
+  def self.hours_as_seconds(stringed_time)
+    time.split(":")[0].to_i * 3600
+  end
+
+  def self.minutes_as_seconds(stringed_time)
+    time.split(":")[1].to_i * 60
+  end
+
+  def self.time_as_seconds(stringed_time)
+    hours_as_seconds(stringed_time) + minutes_as_seconds(stringed_time)
+  end
+
+  def self.change_in_time(start_time, end_time)
+    time_as_seconds(end_time) - time_as_seconds(start_time)
+  end
 end
