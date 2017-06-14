ac91a8f48f95c5ac7191059745179c6c7a4f2dab
timproctor <timandcamiproctor@gmail.com>
2017-06-14 08:48:23 -0600

    not broken, just need to hook up new driver.index within dirt.index

--- rootcare/app/controllers/application_controller.rb
+++ rootcare/app/controllers/application_controller.rb
@@ -28,6 +28,7 @@ class ApplicationController < ActionController::Base
     drivers.map do |driver|
       driver if filtered_trips_stringify.include?(driver.id.to_s) == false
     end.compact
+    byebug
   end
 
   def filter_trips
@@ -37,9 +38,6 @@ class ApplicationController < ActionController::Base
         @filtered_trips << trip
       end
     end
-
-    filter_by_0_distance
-    byebug
   end
 
   def sort_filter_by_most_trip_miles
