17f4468742c5536d84be7ebffd6d008a2f46e4a1
timproctor <timandcamiproctor@gmail.com>
2017-06-22 07:32:56 -0600

    got all specs green

--- rootcare/spec/features/list_trips_spec.rb
+++ rootcare/spec/features/list_trips_spec.rb
@@ -7,14 +7,14 @@ RSpec.feature "Drivers listed", :type => :feature do
 
   scenario "A list of driver names" do
     visit "dirts#index"
-    within "tr:nth-child(1) th:nth-child(1)" do
+    within(:css, "//table/tr[1]/th[1]") do
       expect(page).to have_text("Driver's Name")
     end
   end
 
   scenario "Driver's total miles" do
     visit "dirts#index"
-    within "tr:nth-child(1) th:nth-child(2)" do
+    within(:css, "//table/tr[1]/th[2]") do
       expect(page).to have_text("Total Distance")
     end
   end
@@ -22,7 +22,7 @@ RSpec.feature "Drivers listed", :type => :feature do
   scenario "Driver's average speed for all his trips is displayed" do
     visit "dirts#index"
 
-    within "tr:nth-child(1) th:nth-child(3)" do
+    within(:css, "//table/tr[1]/th[3]") do
       expect(page).to have_text("Average Speed")
     end
   end
@@ -30,19 +30,23 @@ RSpec.feature "Drivers listed", :type => :feature do
   scenario "Don't display trips less than 5mph" do
     visit "dirts#index"
 
-    expect(page).not_to have_text("Tim")
+    within(:css, "//table/tr[8]/td[2]") do
+      expect(page).to have_text("0 miles")
+    end
   end
 
   scenario "Don't display trips more than 100 mph" do
     visit "dirts#index"
 
-    expect(page).not_to have_text("305 Miles")
+    within(:css, "//table/tr[6]/td[2]") do
+      expect(page).to have_text("0 miles")
+    end
   end
 
   scenario "Sorted by driver with most miles driven" do
     visit "dirts#index"
 
-    within "tr:nth-child(2) td:nth-child(1)" do
+    within(:css, "//table/tr[2]/td[1]") do
       expect(page).to have_text("Rex")
     end
   end
@@ -50,26 +54,24 @@ RSpec.feature "Drivers listed", :type => :feature do
   scenario "Miles are rounded to the nearest integer" do
     visit "dirts#index"
 
-    within "tr:nth-child(2) td:nth-child(2)" do
-      expect(page).to have_text("55")
+    within(:css, "//table/tr[2]/td[2]") do
+      expect(page).to have_text("55 miles")
     end
   end
 
   scenario "Speed is rouned to the nearest integer" do
     visit "dirts#index"
 
-    within "tr:nth-child(2) td:nth-child(3)" do
-      expect(page).to have_text("32")
+    within(:css, "//table/tr[2]/td[3]") do
+      expect(page).to have_text("32 miles")
     end
   end
 
   scenario "Drivers without dirts are given 0 miles under total distance" do
     visit "dirts#index"
 
-    within "tr:nth-child(6) td:nth-child(1)" do
-      expect(page).to have_text("Mark")
+    within(:css, "//table/tr[6]/td[2]") do
+      expect(page).to have_text("0 miles")
     end
   end
-
-
 end
